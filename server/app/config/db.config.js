8ad8a857aba8a14c5452af6cc8c81d2642e83761
Minangkara Rengga <49264068+minangkaracr@users.noreply.github.com>
2023-12-19 08:55:56 +0700

    Update db.config.js

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,22 +1,8 @@
-// module.exports = {
-//     HOST: "localhost",
-//     USER: "postgres",
-//     PASSWORD: "admin123",
-//     DB: "postgres",
-//     dialect: "postgres",
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000
-//     }
-//   }
-
 module.exports = {
-  HOST: "10.4.27.13",
-  USER: "ois",
-  PASSWORD: "oismidi",
-  DB: "oisho",
+  HOST: "HOST_SERVER",
+  USER: "USER_SERVER",
+  PASSWORD: "PW_SERVER",
+  DB: "DB_SERVER",
   dialect: "postgres",
   pool: {
     max: 5,
